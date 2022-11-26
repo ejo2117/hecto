652562a385936197aefd7a8f503e219f82500632
Ethan <ejo2117@columbia.edu>
2022-11-26 18:18:28 -0500

    End Chapter 5: Address clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-#![warn(clippy::all, clippy::pedantic)]
+#![warn(clippy::all, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::implicit_return,
+    clippy::shadow_reuse,
+    clippy::print_stdout,
+    clippy::wildcard_enum_match_arm,
+    clippy::else_if_without_else
+)]
 mod document;
 mod editor;
 mod row;
@@ -10,5 +18,6 @@ pub use row::Row;
 pub use terminal::Terminal;
 
 fn main () {
-    Editor::default().run();
+    dbg!(std::usize::MAX);
+    // Editor::default().run();
 }
