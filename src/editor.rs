47c9ac24fd44946e51b34b180cc36d617d801a4c
Ethan <ejo2117@columbia.edu>
2022-11-27 15:12:20 -0500

    Ch5: Quit Confirmation

--- src/editor.rs
+++ src/editor.rs
@@ -11,6 +11,7 @@ use termion::event::Key;
 const STATUS_FG_COLOR: color::LightRed = color::LightRed;
 const STATUS_BG_COLOR: color::Red = color::Red;
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+const QUIT_TIMES: u8 = 3;
 
 #[derive(Default)]
 pub struct Position {
@@ -39,6 +40,7 @@ pub struct Editor {
     offset: Position,
     document: Document,
     status_message: StatusMessage,
+    quit_times: u8,
 }
 
 impl Editor {
@@ -77,6 +79,7 @@ impl Editor {
             cursor_position: Position::default(), 
             offset: Position::default(),
             status_message: StatusMessage::from(initial_status),
+            quit_times: QUIT_TIMES,
         }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
@@ -117,7 +120,17 @@ impl Editor {
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('q') => {
+                if self.quit_times > 0 && self.document.is_dirty() {
+                    self.status_message = StatusMessage::from(format!(
+                        "WARNING! File has unsaved changes. Press Ctrl-Q {} more times to quit.",
+                        self.quit_times
+                    ));
+                    self.quit_times -= 1;
+                    return Ok(());
+                }
+                self.should_quit = true
+            }
             Key::Ctrl('s') => self.save(),
             Key::Char(c) =>  { 
                 self.document.insert(&self.cursor_position, c);
@@ -134,6 +147,10 @@ impl Editor {
             _ => (),
         }
         self.scroll();
+        if self.quit_times < QUIT_TIMES {
+            self.quit_times = QUIT_TIMES;
+            self.status_message = StatusMessage::from(String::new());
+        }
         Ok(())
     }
     fn scroll(&mut self) {
